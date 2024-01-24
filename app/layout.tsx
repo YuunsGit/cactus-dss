3fc765fb88f748469f017e95d19c0bff89d5c0b4
YuunsGit <yunus.emre.kepenek@outlook.com>
2024-01-24 16:40:43 +0300

    Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,23 @@
-import type { Metadata } from "next";
 import { PT_Sans } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/components/providers/auth-provider";
 import { cookies } from "next/headers";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import React from "react";
 
 const pt_sans = PT_Sans({ subsets: ["latin"], weight: ["400", "700"] });
 
-export const metadata: Metadata = {
-  title: "Cactus SIS",
-  description: "Student Information Systems",
+export const metadata = {
+  title: "Cactus DSS",
+  description: "DSS for student information systems.",
+  metadataBase: new URL("https://dss.yunusemre.dev"),
+
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "https://dss.yunusemre.dev/site.webmanifest",
 };
 
 export default function RootLayout({
